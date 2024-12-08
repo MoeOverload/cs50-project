975778d08e9a2f034fcf9fb43566f12e7d4d7d67
MOE <goombaplayz@gmail.com>
2024-12-07 20:30:16 -0800

    buttn hover and push stuff

--- attack-of-the-orcs/ui scenes/difficulty_menu.gd
+++ attack-of-the-orcs/ui scenes/difficulty_menu.gd
@@ -11,18 +11,17 @@ func _on_hardbutton_pressed():
 	Globalscript.easy_mode = false
 	Globalscript.med_mode = false
 	Globalscript.hard_mode = true
-	get_tree().change_scene_to_file("res://ui scenes/level_select.tscn")
+	
 
 
 func _on_medbutton_pressed():
 	Globalscript.easy_mode = false
 	Globalscript.hard_mode = false
 	Globalscript.med_mode = true
-	get_tree().change_scene_to_file("res://ui scenes/level_select.tscn")
-
+	
 
 func _on_easybutton_pressed():
 	Globalscript.med_mode = false
 	Globalscript.hard_mode = false
 	Globalscript.easy_mode = true
-	get_tree().change_scene_to_file("res://ui scenes/level_select.tscn")
+	
