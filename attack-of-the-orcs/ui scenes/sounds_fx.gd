f285d1ff30876044e6a9063f02962dce5326cd01
MOE <goombaplayz@gmail.com>
2024-12-21 17:34:23 -0800

    fixed the sound menu ui

--- attack-of-the-orcs/ui scenes/sounds_fx.gd
+++ attack-of-the-orcs/ui scenes/sounds_fx.gd
@@ -5,8 +5,15 @@ extends Control
 func _on_backbutton_button_up() -> void:
 	get_tree().change_scene_to_file("res://ui scenes/settings_menu.tscn")
 
-
+#master volume control
 func _on_check_button_toggled() :
-	pass 
+	pass
 	
-	bus_layout.audio
+	
+#music mute cntrol
+func _on_musicmute_toggled():
+	pass # Replace with function body.
+
+#sound fx control
+func _on_soundfx_mute_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
