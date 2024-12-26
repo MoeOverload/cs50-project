0f5dd9d09b200e45c4e1259b60ba44d498a06532
MOE <goombaplayz@gmail.com>
2024-12-26 08:09:59 -0800

    stuff

--- attack-of-the-orcs/ui scenes/sounds_fx.gd
+++ attack-of-the-orcs/ui scenes/sounds_fx.gd
@@ -6,7 +6,7 @@ func _on_backbutton_button_up() -> void:
 	get_tree().change_scene_to_file("res://ui scenes/settings_menu.tscn")
 
 #master volume control
-func _on_check_button_toggled() :
+func _on_check_button_toggled():
 	pass
 	
 	
